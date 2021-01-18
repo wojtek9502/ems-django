0c787b1ae9cc36b8da83142dff758ec292ae894a
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-01-18 22:55:18 +0100

    add first model

--- ems_django/ems_api/models.py
+++ ems_django/ems_api/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class EntranceExit(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    entrance_date = models.DateTimeField(auto_now_add=True, blank=False)
+    exit_date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    def __str__(self):
+        return f"User: {self.user}, entrance_date: {str(self.entrance_date)}, exit_date: {str(self.exit_date)}"
