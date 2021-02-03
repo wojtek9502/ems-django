c51e731b3ded73f040377d3563ee33ea59e6f885
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-02-03 21:57:03 +0100

    override list, retrive, destroy methods

--- README.md
+++ README.md
@@ -13,3 +13,31 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
 ```
+
+
+### Get API Token
+```
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "admin", "password": "admin"}' \
+  http://localhost:8000/api/token/
+```
+
+### Refresh token
+
+```
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"refresh":"<REFRESH-TOKEN>"}' \
+  http://localhost:8000/api/token/refresh/
+
+```
+### Get some data
+
+```
+curl \
+  -H "Authorization: Bearer <TOKEN>" \
+  http://127.0.0.1:8000/api/entrance_exit/
+```
--- ems_django/ems_api/views.py
+++ ems_django/ems_api/views.py
@@ -1,12 +1,29 @@
 from rest_framework import viewsets, permissions
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
 from ems_api.models import EntranceExit
 from ems_api.serializers import EntranceExitSerializer
 
 # Create your views here.
-class EntranceExitViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+class EntranceExitViewSet(viewsets.ViewSet):
     queryset = EntranceExit.objects.all()
     serializer_class = EntranceExitSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request):
+        queryset = EntranceExit.objects.order_by("-entrance_date")
+        print(queryset)
+        serializer = EntranceExitSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = EntranceExit.objects.all()
+        entrance_exit_obj = get_object_or_404(queryset, pk=pk)
+        serializer = EntranceExitSerializer(entrance_exit_obj)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        obj = EntranceExit.objects.get(pk=pk)
+        obj.delete()
+        return Response(f"Delete: {obj}")
